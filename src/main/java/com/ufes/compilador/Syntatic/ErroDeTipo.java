f8bc879e2709a6a16b9b2d4badd4092586620082
Danilo J. L. de Oliveira <danilojldeo@hotmail.com>
2023-01-18 10:26:44 -0300

    Start: Escopo Error

--- src/main/java/com/ufes/compilador/Syntatic/ErroDeTipo.java
+++ src/main/java/com/ufes/compilador/Syntatic/ErroDeTipo.java
@@ -1,7 +1,7 @@
 // linha -> tokenList[x].line 
 // valor -> tokenList[x].text 
 // token atribuído -> tokenList[x].token
-// achou o erro -> New Yyerror(linha, descrição do erro);
+// achou o erro -> new Yyerror(linha, descrição do erro);
 
 package com.ufes.compilador.Syntatic;
 
--- src/main/java/com/ufes/compilador/Syntatic/Escopo.java
+++ src/main/java/com/ufes/compilador/Syntatic/Escopo.java
@@ -1,20 +1,53 @@
 // linha -> tokenList[x].line 
 // valor -> tokenList[x].text 
 // token atribuído -> tokenList[x].token
-// achou o erro -> New Yyerror(linha, descrição do erro);
+// achou o erro -> new Yyerror(linha, descrição do erro);
 
 package com.ufes.compilador.Syntatic;
 
+import com.ufes.compilador.JFlex.Yyerror;
 import com.ufes.compilador.Model.tokenCollection;
+import com.ufes.compilador.Model.tokenModel;
+import java.util.List;
 
 /**
  *
- * @author
+ * @author Danilo-Js
  */
 public class Escopo {
     public tokenCollection tokenList;
+    
+    public List<tokenModel> variaveis;
 
+    public static final String token_startVars = "tk_vars";
+    public static final String token_endVars = "tk_inicio";
+    
+    public boolean isCapturingVars = false;
+    
     public Escopo(tokenCollection tokenList) {
         this.tokenList = tokenList;
+        this.isCapturingVars = false;
+        this.verify();
+    }
+    
+    public void checkVar(tokenModel variable) {
+        if (variaveis.contains(variable)) {
+            new Yyerror(variable.line, "Variável sendo declarada mais de uma vez");
+        } else {
+            variaveis.add(variable);
+        }
+    }
+    
+    public void verify() {
+        for(tokenModel tk : tokenList.tokens) {
+            if (this.isCapturingVars) {
+                this.checkVar(tk);
+            }
+            if (tk.token == token_startVars) {
+                this.isCapturingVars = true;
+            } else if (tk.token == token_endVars) {
+                this.isCapturingVars = false;
+            }
+        }
     }
 }
